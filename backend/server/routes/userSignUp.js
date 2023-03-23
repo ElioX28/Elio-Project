fda1b4d03228a8a8cca15bb943dd4418965d702e
Joseph LeMoure <joelem316@gmail.com>
2023-03-23 09:16:59 -0400

    fix favline

--- backend/server/routes/userSignUp.js
+++ backend/server/routes/userSignUp.js
@@ -28,7 +28,7 @@ router.post('/signup', async (req, res) => {
         username: username,
         email: email,
         password: hashPassword,
-        favline : faveline,
+        favline : favline,
         zipcode : zipcode
     });
 
