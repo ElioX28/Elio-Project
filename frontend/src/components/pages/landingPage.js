bfde5202390d66085cf1f9aafa2caef573c2c92f
Ryan Frasier <rfrasier977@gmail.com>
2023-03-27 21:25:32 -0400

    landingPage update

--- frontend/src/components/pages/landingPage.js
+++ frontend/src/components/pages/landingPage.js
@@ -6,8 +6,9 @@ const Landingpage = () => {
     return (
         <Card style={{ width: '30rem' }} className="mx-2 my-2">
         <Card.Body>
-          <Card.Title>Elio's User Skeleton App</Card.Title>
-          <Card.Subtitle className="mb-2 text-muted">A starting point for an application.</Card.Subtitle>
+          <Card.Title>MBTA Transit Scheduling App</Card.Title>
+          <Card.Subtitle className="mb-2 text-muted">
+            A web application designed to help you navigate and schedule your trip on the MBTA.</Card.Subtitle>
           <Card.Text>
           </Card.Text>
           <Card.Link href="/signup">Sign Up</Card.Link>
