7c7db661754695a14c0677944c77c86f050ff135
Joseph LeMoure <joelem316@gmail.com>
2023-04-20 08:36:43 -0400

    homepage UI changes

--- frontend/src/components/pages/homePage.js
+++ frontend/src/components/pages/homePage.js
@@ -1,8 +1,8 @@
 import { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
-import Card from 'react-bootstrap/Card';
 import axios from 'axios';
 import getUserInfo from '../../utilities/decodeJwt';
+import Card from 'react-bootstrap/Card';
 
 const HomePage = () => {
   const [user, setUser] = useState({});
@@ -29,84 +29,67 @@ const HomePage = () => {
     navigate('/');
   };
 
-  if (!user.id) return <div><h4>Log in to view this page.</h4></div>;
+  if (!user.id) return <h4>Please log in to view this page.</h4>;
 
   return (
     <>
-      <div>
-        <h3 class="text-center">
-          Methods of Transportation
-        </h3>
-      </div>
-      <div class="container">
-        <div class="row g-3 justify-content-start"> {/* Add justify-content-start class to move pictures to left */}
-          {[
-            {
-              name: 'Commuter Rail',
-              imgSrc: 'commuterrailicon.png',
-              onClick: () => navigate('/cmlines'),
-            },
-            {
-              name: 'Subway',
-              imgSrc: 'subwayicon.png',
-              onClick: () => navigate('/subwaylines'),
-            },
-            {
-              name: 'Bus',
-              imgSrc: 'busicon.png',
-              onClick: () => navigate('/buslines'),
-            },
-            {
-              name: 'Ferry',
-              imgSrc: 'ferryicon.png',
-              onClick: () => navigate('/ferrylines'),
-            },
-          ].map((transportation, index) => (
-            <div class="col-12 col-md-6 col-lg-4" key={index}>
-              <div clas="card">
-                <button class="btn-warning" onClick={transportation.onClick}>
-                  <h5 clas="card-title "></h5>
-                  <img src={transportation.imgSrc} alt={transportation.name} class="card-img-top"></img>
-                  <div class="card-body"></div>
+      <div className="container">
+        <h3 className="text-center my-5">Methods of Transportation</h3>
+        <div className="row row-cols-1 row-cols-md-2 row-cols-lg-4 g-4 justify-content-center">
+          {[{name: "Commuter Rail",imgSrc: "commuterrailicon.png",onClick: () => navigate("/cmlines"), color: "#84246c"},{name: "Subway",imgSrc: "subwayicon.png",onClick: () => navigate("/subwaylines"),color: "#4c4c5c"},{name: "Bus",imgSrc: "busicon.png",onClick: () => navigate("/buslines"),color: "#fac32c"},{name: "Ferry",imgSrc: "ferryicon.png",onClick: () => navigate("/ferrylines"),color: "#058cac"},].map((transportation, index) => (
+            <div className="col" key={index} style={{ width: "325px" }}>
+              <div className="card" style={{ height: "375px" }}>
+                <button
+                  className="btn"
+                  onClick={transportation.onClick}
+                  style={{ width: "100%", height: "100%", backgroundColor: transportation.color }}
+                >
+                  <img
+                    src={transportation.imgSrc}
+                    alt={transportation.name}
+                    className="card-img-top"
+                    style={{ width: "100%", height: "75%" }}
+                  />
                 </button>
               </div>
             </div>
           ))}
         </div>
-        <div class="float-end"> {/* Add float-end class to move alerts to right */}
-          <div style={{ height: '900px', overflowY: 'scroll' }}>
-            {alerts.map((alert, index) => (
-              <Card
-                key={index}
-                body
-                outline
-                color="success"
-                className="mx-1 my-2"
-                style={{ width: '30rem' }}
-              >
-                <div class="alert alert-info">
-                  <Card.Body>
-                    <Card.Title>Alert</Card.Title>
-                    <Card.Text>
+        <div className="my-5">
+          <h3 className="text-center">Alerts</h3>
+          {alerts.map((alert, index) => (
+            <Card
+              key={index}
+              body
+              outline
+              color="success"
+              className="mx-1 my-2"
+              style={{ width: "30rem" }}
+            >
+              <div className="alert alert-info">
+                <Card.Body>
+                  <Card.Title>Alert</Card.Title>
+                  <Card.Text>
                     {alert.attributes.header + alert.attributes.description}
-                    </Card.Text>
-                    <Card.Text>
-                    Time: {new Date(alert.attributes.updated_at).toLocaleString()}
-                    </Card.Text>
-
-                  </Card.Body>
-                </div>
-              </Card>
-            ))}
-          </div>
+                  </Card.Text>
+                  <Card.Text>
+                    Time:{" "}
+                    {new Date(alert.attributes.updated_at).toLocaleString()}
+                  </Card.Text>
+                </Card.Body>
+              </div>
+            </Card>
+          ))}
         </div>
       </div>
-      <button onClick={handleClick}>
-        Log Out
-
-      </button>
+      <div className="d-flex justify-content-center my-5">
+        <button className="btn btn-danger" onClick={handleClick}>
+          Log Out
+        </button>
+      </div>
     </>
   );
+  
 };
 
 export default HomePage;
