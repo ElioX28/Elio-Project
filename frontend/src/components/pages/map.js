f39bb98785ee0ff0bf0841ee52bf59e94ef5e62d
ElioX28 <elioaxhufka@gmail.com>
2023-04-26 20:39:50 -0400

    Interactive map

--- frontend/src/components/pages/map.js
+++ frontend/src/components/pages/map.js
@@ -1,12 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import 'leaflet/dist/leaflet.css';
+import stationsData from '/Users/kozetadimo/source/repos/Elio-Project/frontend/src/stations.json';
 
 export default function Map() {
-    return (
-      <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
-        <img src= "/mbtamap1.png" alt="MBTA Map" style={{ width: '50%', height: 'auto' }} />
-        <img src= "/mbta+cr.png" alt="MBTA Map" style={{ width: '50%', height: 'auto' }} />
-      </div>
-      
-    );
-  }
+  const [stations, setStations] = useState(stationsData);
+
+  return (
+    <MapContainer center={[42.3601, -71.0589]} zoom={13} style={{ height: '100vh', width: '100vw' }}>
+      <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+      {Array.isArray(stations) && stations.map((station) => (
+        <Marker key={station.id} position={[station.latitude, station.longitude]}>
+          <Popup>{station.name}</Popup>
+        </Marker>
+      ))}
+    </MapContainer>
+  );
+}
 
